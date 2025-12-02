e50dc5c817922c5e8c8b804423859592df4a69b1
Ian Schmitz <ianschmitz@gmail.com>
2025-12-01 21:58:28 -0800

    Day 1 WIP

---
+++ 2025/01/aoc.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+)
+
+const (
+	part1Expected = 3
+	part2Expected = 6
+	dialSize      = 100
+)
+
+func main() {
+	runTest()
+	run()
+}
+
+func runTest() {
+	part1, part2 := solve("./input_test.txt")
+	if part1 != part1Expected {
+		log.Fatalf("Part 1 - Expected: %d, receieved: %d", part1Expected, part1)
+	}
+	if part2 != part2Expected {
+		log.Fatalf("Part 2 - Expected: %d, receieved: %d", part2Expected, part2)
+	}
+}
+
+func run() {
+	part1, part2 := solve("./input.txt")
+	fmt.Println("Part 1 - Answer is:", part1)
+	fmt.Println("Part 2 - Answer is:", part2)
+}
+
+func solve(filePath string) (int, int) {
+	file, err := os.Open(getAbsFilePath(filePath))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	dial := 50
+	part1 := 0
+	part2 := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		direction := line[0:1]
+		num, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal("Error converting line to int:", err)
+		}
+
+		if direction == "L" {
+			num = -num
+		}
+
+		newPosition := dial + num
+
+		numTimesPastZero := int(math.Abs(float64(newPosition / dialSize)))
+		part2 += numTimesPastZero
+		// Note: If dial was at 0, and we just moved 1 to the left, we wouldn't count that as having hit 0
+		if numTimesPastZero <= 0 && dial != 0 {
+			part2++
+		}
+
+		dial = newPosition % dialSize
+
+		if dial == 0 {
+			part1++
+		}
+	}
+
+	return part1, part2
+}
+
+func getAbsFilePath(path string) string {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatal("Error getting caller information")
+	}
+
+	currentDir := filepath.Dir(filename)
+
+	return filepath.Join(currentDir, path)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ianschmitz/advent-of-code
+
+go 1.25.4
